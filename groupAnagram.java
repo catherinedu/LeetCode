122f140ffac0b5e46dec672b4864181d9cbcae96
Catherine Du <yd2386@columbia.edu>
2018-08-07 00:24:58 +0300

    Add files via upload
    LC49

---
+++ groupAnagram.java
@@ -0,0 +1,13 @@
+public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0) return new ArrayList();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if(!map.containsKey(key)) map.put(key, new ArrayList());
+            map.get(key).add(s);//add the original string to the arraylist;
+        }
+    
+        return new ArrayList(map.values());
+    }
