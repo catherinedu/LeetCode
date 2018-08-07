293cd5297800cdb5cc7b454597704d4b5827b9bd
Catherine Du <yd2386@columbia.edu>
2018-08-07 14:54:24 +0300

    Add files via upload

---
+++ 3Sum.java
@@ -0,0 +1,36 @@
+public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null ||nums.length < 3){
+            return result;
+        }
+        Arrays.sort(nums);
+         
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i == 0 || nums[i] > nums[i - 1]){
+                int j = i + 1; //middle
+                int k = nums.length - 1; //end
+                while(j < k){
+                    if(nums[i] + nums[k] + nums[j] == 0){
+                        result.add(new ArrayList<>(Arrays.asList(nums[i], nums[k], nums[j])));
+                        j++;
+                        k--;
+                        
+                        while(j < k && nums[j] == nums[j-1]){
+                            j++;
+                        }
+                        while(j < k && nums[k] == nums[k + 1]){
+                            k--;
+                        }
+                    }
+                    else if(nums[i] + nums[j] + nums[k] < 0){
+                        j++;
+                    }
+                    else{
+                        k--;
+                    }
+                }
+            }
+        }
+        return result;
+        
+    }
